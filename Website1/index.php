071c4a4517f7cde87941896908161613350944b0
onesixromcom <16732145+onesixromcom@users.noreply.github.com>
2021-06-01 15:12:44 +0300

    Docker configs, websites, variables

---
+++ Website1/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <title>Website 1</title>
+</head>
+<body>
+<h1><?php echo "Website 1"?></h1>
+<?php phpinfo();?>
+</body>
+</html>
---
+++ Website2/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <title>Website 2</title>
+</head>
+<body>
+<h1><?php echo "Website 2"?></h1>
+<?php phpinfo();?>
+</body>
+</html>
